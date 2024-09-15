842910ab388e3b4ac3058a8e3c8ffd4dcd0a136f
UrielArias <urielarias.a12@gmail.com>
2024-09-15 17:25:13 -0300

    refactor(server.models): changing Appointment class attributes' visibility to private. Getters and setters added.

--- server/src/main/java/ar/edu/itba/pod/server/models/Appointment.java
+++ server/src/main/java/ar/edu/itba/pod/server/models/Appointment.java
@@ -1,13 +1,37 @@
 package ar.edu.itba.pod.server.models;
 
 public class Appointment {
-    long roomId;
-    Patient patient;
-    Doctor doctor;
+    private long roomId;
+    private Patient patient;
+    private Doctor doctor;
 
     public Appointment(long roomId, Patient patient, Doctor doctor){
         this.doctor = doctor;
         this.roomId = roomId;
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public long getRoomId() {
+        return roomId;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public void setPatient(Patient patient) {
         this.patient = patient;
     }
 }
