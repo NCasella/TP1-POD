6431e2b1398bd3afec9123ca5cae6448fd61f0da
nicol <nicolascasella02@gmail.com>
2024-09-16 00:45:43 -0300

    completed implmeneting adminService methods

--- server/src/main/java/ar/edu/itba/pod/server/models/Level.java
+++ server/src/main/java/ar/edu/itba/pod/server/models/Level.java
@@ -1,9 +1,23 @@
 package ar.edu.itba.pod.server.models;
 
+import java.util.Arrays;
+
 public enum Level {
-    LEVEL_1,
-    LEVEL_2,
-    LEVEL_3,
-    LEVEL_4,
-    LEVEL_5
+    LEVEL_1(1),
+    LEVEL_2(2),
+    LEVEL_3(3),
+    LEVEL_4(4),
+    LEVEL_5(5);
+
+    private final int levelNumber;
+
+     Level(int levelNumber){
+        this.levelNumber=levelNumber;
+    }
+
+    public static Level getLevelFromNumber(int levelNumber){
+        return Arrays.stream(Level.values()).filter((level -> level.levelNumber==levelNumber))
+                .findFirst().orElseThrow(IllegalArgumentException::new);
+    }
+    public  int getLevelNumber(){return this.levelNumber;}
 }
--- server/src/main/java/ar/edu/itba/pod/server/services/AdminService.java
+++ server/src/main/java/ar/edu/itba/pod/server/services/AdminService.java
@@ -2,6 +2,7 @@ package ar.edu.itba.pod.server.services;
 
 import ar.edu.itba.pod.grpc.AdminServiceGrpc;
 import ar.edu.itba.pod.grpc.Service;
+import ar.edu.itba.pod.server.models.Disponibility;
 import ar.edu.itba.pod.server.models.Doctor;
 import ar.edu.itba.pod.server.models.Level;
 import ar.edu.itba.pod.server.repositories.DoctorRepository;
@@ -11,8 +12,6 @@ import com.google.protobuf.StringValue;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Objects;
 
 public class AdminService extends AdminServiceGrpc.AdminServiceImplBase {
     private final DoctorRepository doctorRepository;
@@ -26,29 +25,31 @@ public class AdminService extends AdminServiceGrpc.AdminServiceImplBase {
 
     @Override
     public void addDoctor(Service.EnrollmentInfo request, StreamObserver<StringValue> responseObserver) {
-
         String doctorName=request.getName();
         if(request.getLevel().getNumber()==0)
             throw new IllegalArgumentException("Invalid level parameter");
         Level doctorLevel=Level.valueOf(request.getLevel().toString());
         doctorRepository.addDoctor(doctorName,doctorLevel);
         LOGGER.info("Doctor {} added to register",doctorName);
-        responseObserver.onNext(StringValue.of(String.format("Doctor %s (%d) added successfully",doctorName,doctorLevel.ordinal())));
+        responseObserver.onNext(StringValue.of(String.format("Doctor %s (%d) added successfully",doctorName,doctorLevel.getLevelNumber())));
         responseObserver.onCompleted();
     }
 
     @Override
     public void addRoom(Empty request, StreamObserver<Service.Id> responseObserver) {
-        super.addRoom(request, responseObserver);
+        responseObserver.onNext(Service.Id.newBuilder().setId(roomsRepository.addRoom()).build());
+        responseObserver.onCompleted();
     }
 
     @Override
-    public void setDoctor(Service.DoctorAvailabilityInfo request, StreamObserver<Service.DoctorAvailabilityInfo> responseObserver) {
-        super.setDoctor(request, responseObserver);
+    public void setDoctor(Service.DoctorAvailabilityInfo request, StreamObserver<Empty> responseObserver) {
+        doctorRepository.setDoctorDisponibility(request.getName(), Disponibility.getDisponibilityFromNumber(request.getAvailability().getNumber()));
     }
 
     @Override
     public void checkDoctor(StringValue request, StreamObserver<Service.DoctorAvailabilityInfo> responseObserver) {
-        super.checkDoctor(request, responseObserver);
+        Doctor doctor=doctorRepository.getDoctor(request.getValue());
+        responseObserver.onNext(Service.DoctorAvailabilityInfo.newBuilder().setName(doctor.getDoctorName()).setAvailabilityValue(doctor.getDisponibility().ordinal()).build());
+        responseObserver.onCompleted();
     }
 }
