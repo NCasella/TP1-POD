28c2b8f1e8f73987bf1b6a171667fb41163791b0
NCasella <nicolascasella02@gmail.com>
2024-09-17 18:30:18 -0300

    changed responses of services to StringValue

--- server/src/main/java/ar/edu/itba/pod/server/models/Disponibility.java
+++ server/src/main/java/ar/edu/itba/pod/server/models/Disponibility.java
@@ -3,15 +3,19 @@ package ar.edu.itba.pod.server.models;
 import java.util.Arrays;
 
 public enum Disponibility {
-    AVAILABLE(1),
-    UNAVAILABLE(2),
-    ATTENDING(3);
+    AVAILABLE(1,"Available"),
+    UNAVAILABLE(2,"Unavailable"),
+    ATTENDING(3,"Attending");
 
     private final int disponibilityNumber;
-    Disponibility(int disponibilityNumber){
+    private final String disponibilityString;
+
+    Disponibility(int disponibilityNumber,String disponibilityString){
         this.disponibilityNumber=disponibilityNumber;
+        this.disponibilityString=disponibilityString;
     }
 
+    public String getDisponibilityString(){return this.disponibilityString;}
     public static Disponibility getDisponibilityFromNumber(int disponibilityNumber){
         return Arrays.stream(Disponibility.values())
                 .filter((disponibility -> disponibility.disponibilityNumber==disponibilityNumber)).findAny().orElseThrow(IllegalArgumentException::new);
