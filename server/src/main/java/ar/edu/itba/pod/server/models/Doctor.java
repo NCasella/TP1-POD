498bb983aeecd1214ce10089b12a9e2428aba570
UrielArias <urielarias.a12@gmail.com>
2024-09-21 23:56:06 -0300

    refactor(server.models): adding locks to ensure atomicity in level and disponibility changes for patient and doctor.

--- server/src/main/java/ar/edu/itba/pod/server/models/Doctor.java
+++ server/src/main/java/ar/edu/itba/pod/server/models/Doctor.java
@@ -1,11 +1,14 @@
 package ar.edu.itba.pod.server.models;
 
 import java.util.Objects;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Doctor {
     private final String doctorName;
     private Level level;
     private Availability disponibility;
+    private final Lock isModifiable = new ReentrantLock(true);
 
     public Doctor(String doctorName, Level level) {
         this.doctorName = doctorName;
@@ -13,22 +16,37 @@ public class Doctor {
         this.disponibility=Availability.AVAILABLE;
     }
 
+    public void lockDoctor(){
+        isModifiable.lock();
+    }
+
+    public void unlockDoctor(){
+        isModifiable.unlock();
+    }
     public String getDoctorName() {
         return doctorName;
     }
     public void setDisponibility(Availability disponibility){
+        isModifiable.lock();
         this.disponibility=disponibility;
+        isModifiable.unlock();
     }
     public Availability getDisponibility(){
+//        isModifiable.lock();
+//        isModifiable.unlock();
         return this.disponibility;
     }
 
     public Level getLevel() {
+//        isModifiable.lock();
+//        isModifiable.unlock();
         return level;
     }
 
     public void setLevel(Level level) {
+        isModifiable.lock();
         this.level = level;
+        isModifiable.unlock();
     }
 
     @Override
--- server/src/main/java/ar/edu/itba/pod/server/models/Patient.java
+++ server/src/main/java/ar/edu/itba/pod/server/models/Patient.java
@@ -2,24 +2,34 @@ package ar.edu.itba.pod.server.models;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Patient {
 
    private final String name;
    private Level level;
    private final LocalDateTime arrivalTime; //arrival time or id based on arrival time
-
+   private final Lock  isModifiable = new ReentrantLock(true);
    public Patient(String name, Level level, LocalDateTime arrivalTime){
        this.name = name;
        this.level = level;
        this.arrivalTime = arrivalTime;
    }
 
+   public void lockPatient(){
+       isModifiable.lock();
+   }
+   public void unlockPatient(){
+       isModifiable.unlock();
+   }
     public String getPatientName() {
         return name;
     }
 
     public Level getPatientLevel(){
+//       isModifiable.lock();
+//       isModifiable.unlock();
        return level;
     }
 
@@ -37,7 +47,11 @@ public class Patient {
        }
        return this.name.equals(patient.name);
     }
-    public void setLevel(Level level){this.level=level;}
+    public void setLevel(Level level){
+       isModifiable.lock();
+       this.level=level;
+       isModifiable.unlock();
+   }
     @Override
     public int hashCode() {
        return Objects.hashCode(name);
